5e78b2c93d3c8307f22a2b23d6b0369c383a5063
mfadl1 <m.fadli.gunardi@gmail.com>
2022-11-03 08:44:36 +0700

    add gui

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
+
+class GUI(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("gui.ui", self)
+
+        self.fitButton.clicked.connect(self.fitData)
+        self.predictButton.clicked.connect(self.predict)
+
+    def fitData(self):
+        ''' TODO: Preprocessing data & fit data here ''' 
+    
+    def predict(self):
+        ''' TODO: Predict output from input arguments ''' 
+        pm10 = self.pm10_input.text()
+        so2 = self.so2_input.text()
+        o3 = self.o3_input.text()
+        no2 = self.no2_input.text()
+        max = self.max_input.text()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    gui = GUI()
+    gui.show()
+
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print("closing window...")
