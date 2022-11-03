b99d3f06314d47366bd590c82d22580fa6f2f326
Muhammad Rayhan Ravianda <rayhanrrav@gmail.com>
2022-11-03 20:13:40 +0700

    integration ml model with gui

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic
+from joblib import load
+from sklearn import preprocessing
 
 class GUI(QWidget):
     def __init__(self):
@@ -8,14 +10,21 @@ class GUI(QWidget):
         uic.loadUi('gui.ui', self)
 
         self.predictButton.clicked.connect(self.predict)
+        self.listkategori = ["Good","Mid","Bad"]
 
     
     def predict(self):
         ''' TODO: Predict output from input arguments ''' 
         pm10 = self.pm10_input.text()
         so2 = self.so2_input.text()
+        # co = self.co_input.text()
         o3 = self.o3_input.text()
         no2 = self.no2_input.text()
+
+        le = preprocessing.LabelEncoder()
+        clfpred = load('model/tutorprediklele.joblib')
+        print(self.listkategori[clfpred.predict([[pm10,so2,10,o3,no2]])[0]])
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
